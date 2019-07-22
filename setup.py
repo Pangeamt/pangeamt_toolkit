686f9d5e6d3bc0e99c955a879b7b9643bbb18498
Aleix <333aleix333@gmail.com>
2019-07-22 16:21:53 +0200

    Version 0.7

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 setup(
   name = 'pangeamt_toolkit',
   packages = ['pangeamt_toolkit'],
-  version = '0.5',
+  version = '0.7',
   license='MIT',
   description = 'PANGEAMT-TOOLKIT',
   author = 'PANGEAMT',
   author_email = 'a.cerda@pangeanic.es',
   url = 'https://github.com/Pangeamt/pangeamt_toolkit',
-  download_url = 'https://github.com/Pangeamt/pangeamt_toolkit/archive/0.5.tar.gz',
-  install_requires=[            # I get to this in a second
+  download_url = 'https://github.com/Pangeamt/pangeamt_toolkit/archive/0.7.tar.gz',
+  install_requires=[
             'Click',
             'joblib',
             'PyYAML',
