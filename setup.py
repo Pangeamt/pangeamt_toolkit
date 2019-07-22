6f4d9baa87ab19e4c9fed50221cddbec0d663232
Aleix <333aleix333@gmail.com>
2019-07-22 15:36:36 +0200

    Version 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'pangeamt_toolkit',
   packages = ['pangeamt_toolkit'],
-  version = '0.2',
+  version = '0.3',
   license='MIT',
   description = 'PANGEAMT-TOOLKIT',
   author = 'PANGEAMT',
