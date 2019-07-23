1423b44090f6120b820c69340b6156cb344322a7
Aleix <333aleix333@gmail.com>
2019-07-23 17:30:06 +0200

    Now pipeline works with two config files

--- pangeamt_toolkit/process_pipeline.py
+++ pangeamt_toolkit/process_pipeline.py
@@ -2,17 +2,34 @@ import yaml as _yaml
 from pangeamt_toolkit.seg import Seg
 
 class Pipeline:
-    def __init__(self, config):
-        with open(config, 'r') as file:
+    def __init__(self, processors, processors_config):
+        """ processors is a list of dicts from the general config file
+            with this structure:
+                {
+                    'name': processor's shortname,
+                    'args': [list of the processor's args]
+                }
+
+            processors_config is a path to a yaml file that contains a
+            dict with this structure:
+                {
+                    'processor's shortname': {
+                        'class': 'processor's class name',
+                        'path': 'processor's path'
+                    }
+                }
+        """
+        with open(processors_config, 'r') as file:
             self._config = _yaml.safe_load(file)
         self._processes = []
-        for process in self._config:
-            klass_name = process['class']
-            path = process['path']
+        for process in processors:
+            processor = self._config[process['name']]
+            klass_name = processor['class']
+            path = processor['path']
             args = process['args']
             mod = __import__(path, fromlist=[klass_name])
             klass = getattr(mod, klass_name)
-            processor = klass(**args)
+            processor = klass(*args)
             self._processes.append(processor)
 
     def preprocess(self, seg):
