f42396652fedaf84a3d7943da7f8516cb0170ce3
Aleix <333aleix333@gmail.com>
2019-07-24 09:44:39 +0200

    Bug fix

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'pangeamt_toolkit',
   packages = ['pangeamt_toolkit'],
-  version = '1.2',
+  version = '1.3',
   license='MIT',
   description = 'PANGEAMT-TOOLKIT',
   author = 'PANGEAMT',
   author_email = 'a.cerda@pangeanic.es',
   url = 'https://github.com/Pangeamt/pangeamt_toolkit',
-  download_url = 'https://github.com/Pangeamt/pangeamt_toolkit/archive/1.2.tar.gz',
+  download_url = 'https://github.com/Pangeamt/pangeamt_toolkit/archive/1.3.tar.gz',
   install_requires=[
             'Click',
             'joblib',
