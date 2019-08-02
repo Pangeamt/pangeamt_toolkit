a268bfa7f6b770362ec83ac650b59bf91c10cd77
Aleix <333aleix333@gmail.com>
2019-08-02 11:31:17 +0200

    Change version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'pangeamt_toolkit',
   packages = ['pangeamt_toolkit'],
-  version = '1.5',
+  version = '1.6',
   license='MIT',
   description = 'PANGEAMT-TOOLKIT',
   author = 'PANGEAMT',
   author_email = 'a.cerda@pangeanic.es',
   url = 'https://github.com/Pangeamt/pangeamt_toolkit',
-  download_url = 'https://github.com/Pangeamt/pangeamt_toolkit/archive/1.5.tar.gz',
+  download_url = 'https://github.com/Pangeamt/pangeamt_toolkit/archive/1.6.tar.gz',
   install_requires=[
             'Click',
             'joblib',
