d54562b8998f17f246276cc6e1c910611c8161bb
Aleix <333aleix333@gmail.com>
2019-12-04 15:43:06 +0100

    Add support for variable train steps

--- pangeamt_toolkit/pangeanmt/pangeanmt.py
+++ pangeamt_toolkit/pangeanmt/pangeanmt.py
@@ -14,11 +14,10 @@ import onmt.model_builder
 import onmt.translate.beam
 import onmt.inputters as inputters
 import onmt.decoders.ensemble
-from onmt.inputters.text_dataset import  TextDataReader
+from onmt.inputters.text_dataset import TextDataReader
 from pangeamt_toolkit.pangeanmt.translation import Translation
 from pangeamt_toolkit.pangeanmt.attention_matrix import AttentionMatrix
-from pangeamt_toolkit.pangeanmt.extended_model.extended_model\
-    import ExtendedModel
+from pangeamt_toolkit.pangeanmt.extended_model.extended_model import ExtendedModel
 from pangeamt_toolkit.pangeanmt.onmtx_translator import OnmtxTranslator
 from onmt.utils.misc import use_gpu
 
@@ -45,19 +44,15 @@ class Pangeanmt:
 
         # Get the model options
         model_path = self._opts.models[0]
-        checkpoint = torch.load(
-            model_path,
-            map_location=lambda storage, loc: storage)
-        self._model_opts = ArgumentParser.ckpt_model_opts(checkpoint['opt'])
+        checkpoint = torch.load(model_path, map_location=lambda storage, loc: storage)
+        self._model_opts = ArgumentParser.ckpt_model_opts(checkpoint["opt"])
         ArgumentParser.update_model_opts(self._model_opts)
         ArgumentParser.validate_model_opts(self._model_opts)
 
         # Extract vocabulary
-        vocab = checkpoint['vocab']
+        vocab = checkpoint["vocab"]
         if inputters.old_style_vocab(vocab):
-            self._fields = inputters.load_old_vocab(
-                vocab, "text", dynamic_dict=False
-            )
+            self._fields = inputters.load_old_vocab(vocab, "text", dynamic_dict=False)
         else:
             self._fields = vocab
 
@@ -70,7 +65,7 @@ class Pangeanmt:
             self._fields,
             use_gpu(self._opts),
             checkpoint,
-            self._opts.gpu
+            self._opts.gpu,
         )
 
         # Translator
@@ -105,60 +100,60 @@ class Pangeanmt:
             beam_size=beam_size,
             replace_unk=replace_unk,
             copy_attn=self._model_opts.copy_attn,
-            global_scorer= GNMTGlobalScorer(0.0, -0.0, 'none', 'none'),
-            seed=self.SEED
+            global_scorer=GNMTGlobalScorer(0.0, -0.0, "none", "none"),
+            seed=self.SEED,
         )
-
 
         online_learning = self._config.online_learning
         if online_learning:
             # Optim
             optimizer_opt = type("", (), {})()
-            optimizer_opt.optim = 'sgd'
+            optimizer_opt.optim = "sgd"
             optimizer_opt.learning_rate = self._opts.learning_rate
-            optimizer_opt.train_from = ''
+            optimizer_opt.train_from = ""
             optimizer_opt.adam_beta1 = 0
             optimizer_opt.adam_beta2 = 0
-            optimizer_opt.model_dtype = 'fp32'
-            optimizer_opt.decay_method = 'none'
+            optimizer_opt.model_dtype = "fp32"
+            optimizer_opt.decay_method = "none"
             optimizer_opt.start_decay_steps = 100000
-            optimizer_opt.learning_rate_decay=1.0
+            optimizer_opt.learning_rate_decay = 1.0
             optimizer_opt.decay_steps = 100000
-            optimizer_opt.max_grad_norm=5
-            self._optim =\
-                Optimizer.from_opt(self._opennmt_model, optimizer_opt,
-                    checkpoint=None)
+            optimizer_opt.max_grad_norm = 5
+            self._optim = Optimizer.from_opt(
+                self._opennmt_model, optimizer_opt, checkpoint=None
+            )
 
             trainer_opt = type("", (), {})()
             trainer_opt.lambda_coverage = 0.0
-            trainer_opt.copy_attn=False
-            trainer_opt.label_smoothing=0.0
-            trainer_opt.truncated_decoder=0
-            trainer_opt.model_dtype='fp32'
-            trainer_opt.max_generator_batches=32
-            trainer_opt.normalization='sents'
-            trainer_opt.accum_count=[1]
-            trainer_opt.accum_steps=[0]
-            trainer_opt.world_size=1
-            trainer_opt.average_decay=0
-            trainer_opt.average_every=1
-            trainer_opt.dropout= 0
-            trainer_opt.dropout_steps=0,
-            trainer_opt.gpu_verbose_level=0
-            trainer_opt.early_stopping=0
-            trainer_opt.early_stopping_criteria=None,
-            trainer_opt.tensorboard=False
-            trainer_opt.report_every=50
-            trainer_opt.gpu_ranks=[]
+            trainer_opt.copy_attn = False
+            trainer_opt.label_smoothing = 0.0
+            trainer_opt.truncated_decoder = 0
+            trainer_opt.model_dtype = "fp32"
+            trainer_opt.max_generator_batches = 32
+            trainer_opt.normalization = "sents"
+            trainer_opt.accum_count = [1]
+            trainer_opt.accum_steps = [0]
+            trainer_opt.world_size = 1
+            trainer_opt.average_decay = 0
+            trainer_opt.average_every = 1
+            trainer_opt.dropout = 0
+            trainer_opt.dropout_steps = (0,)
+            trainer_opt.gpu_verbose_level = 0
+            trainer_opt.early_stopping = 0
+            trainer_opt.early_stopping_criteria = (None,)
+            trainer_opt.tensorboard = False
+            trainer_opt.report_every = 50
+            trainer_opt.gpu_ranks = []
             if self._opts.gpu != -1:
-                trainer_opt.gpu_ranks= [self._opts.gpu]
+                trainer_opt.gpu_ranks = [self._opts.gpu]
 
             self._trainer = build_trainer(
                 trainer_opt,
                 self._opts.gpu,
                 self._opennmt_model,
                 self._fields,
-                self._optim)
+                self._optim,
+            )
         else:
             self._trainer = None
 
@@ -173,7 +168,7 @@ class Pangeanmt:
             data=[("src", srcs)],
             dirs=[None],
             sort_key=inputters.str2sortkey[self._translator.data_type],
-            filter_pred=self._translator._filter_pred
+            filter_pred=self._translator._filter_pred,
         )
 
         data_iter = inputters.OrderedIterator(
@@ -183,25 +178,23 @@ class Pangeanmt:
             train=False,
             sort=False,
             sort_within_batch=False,
-            shuffle=False
+            shuffle=False,
         )
 
         # Translation builder
         translation_builder = onmt.translate.TranslationBuilder(
             dataset,
             self._fields,
-            1,      # NBest
-            True,   # replace_unk,
+            1,  # NBest
+            True,  # replace_unk,
             False,
-            "",     # Phrase table
+            "",  # Phrase table
         )
 
         results = []
         for batch in data_iter:
             batch_data = self._translator.translate_batch(
-                batch,
-                dataset.src_vocabs,
-                True
+                batch, dataset.src_vocabs, True
             )
             translations = translation_builder.from_batch(batch_data)
             for translation in translations:
@@ -209,42 +202,34 @@ class Pangeanmt:
                 src_tokens = translation.src_raw
                 tgt_tokens = translation.pred_sents[0]
 
-
-                #print(f'src_tokens: {src_tokens}')
-                #print(f'tgt_tokens: {tgt_tokens}')
+                # print(f'src_tokens: {src_tokens}')
+                # print(f'tgt_tokens: {tgt_tokens}')
                 # Create src in tgt from tokens
-                #src = ' '.join(src_tokens)
-                #tgt = ' '.join(tgt_tokens)
+                # src = ' '.join(src_tokens)
+                # tgt = ' '.join(tgt_tokens)
 
                 # Create attention matrix
                 attention_matrix = AttentionMatrix(
-                    src_tokens,
-                    tgt_tokens,
-                    translation.attns[0].tolist()
+                    src_tokens, tgt_tokens, translation.attns[0].tolist()
                 )
                 # Score
                 score = translation.pred_scores[0].item()
 
                 # Create a translation object
-                t = Translation(
-                    src_tokens,
-                    tgt_tokens,
-                    attention_matrix,
-                    score
-                )
+                t = Translation(src_tokens, tgt_tokens, attention_matrix, score)
                 # Append to result
                 results.append(t)
         return results
 
     # Train
-    def train (self, src, tgt):
+    def train(self, src, tgt, num_steps):
         online_learning = self._config.online_learning
         # Check if online learning is active
         if not online_learning.is_active:
-            msg = f'Online learning is not active'
+            msg = f"Online learning is not active"
             raise ValueError(msg)
 
-        for _ in range(online_learning.steps):
+        for _ in range(num_steps):
             # Set model mode to train
             self._opennmt_model.train(True)
             # self._model.generator.train()
@@ -262,12 +247,13 @@ class Pangeanmt:
                 data=[("src", srcs), ("tgt", tgts)],
                 dirs=[None, None],
                 sort_key=None,
-                filter_pred=None
+                filter_pred=None,
             )
 
             # Create batches
-            batches = inputters.OrderedIterator(dataset, batch_size=1,
-                device=self._translator._dev)
+            batches = inputters.OrderedIterator(
+                dataset, batch_size=1, device=self._translator._dev
+            )
 
             # Update train steps
             self._train_steps += 1
@@ -278,31 +264,32 @@ class Pangeanmt:
                 self._train_steps,
                 save_checkpoint_steps=None,
                 valid_iter=None,
-                valid_steps=None)
+                valid_steps=None,
+            )
 
     def save_model(self, new_model_dir):
         try:
-            #new_model_dir = os.path.abspath(new_model_dir)
-            #if os.path.isdir(new_model_dir):
+            # new_model_dir = os.path.abspath(new_model_dir)
+            # if os.path.isdir(new_model_dir):
             #    if not os.listdir:
             #        os.rmdir(new_model_dir)
             #    else:
             #        msg = f"{new_model_dir} already exists"
             #        raise ValueError(msg)
             os.mkdir(new_model_dir)
-            #copytree(self._model_dir, new_model_dir, \
+            # copytree(self._model_dir, new_model_dir, \
             #    ignore=ignore_patterns('*.pt'))
-            #model_filename = os.path.basename(\
+            # model_filename = os.path.basename(\
             #    self._opennmt_model.config.opts.models[0])
-            #model_filename_base = re.sub('_[0-9]*\.pt$', '', model_filename )
-            base_path = os.path.join(new_model_dir, 'model_saved')
+            # model_filename_base = re.sub('_[0-9]*\.pt$', '', model_filename )
+            base_path = os.path.join(new_model_dir, "model_saved")
             model_saver = ModelSaver(
                 base_path,
                 self._opennmt_model,
                 self._model_opts,
                 self._fields,
                 self._optim,
-                keep_checkpoint=-1
+                keep_checkpoint=-1,
             )
             model_saver.save(self._train_steps)
         except Exception as e:
--- pangeamt_toolkit/pangeanmt/request_handler/train.py
+++ pangeamt_toolkit/pangeanmt/request_handler/train.py
@@ -1,48 +1,54 @@
 from aiohttp import web
 import time
 
+
 async def train(req):
-    nmt = req.app['nmt']
-    pipeline = req.app['pipeline']
-    pipeline_tgt = req.app['pipeline_tgt']
-    lock = req.app['lock']
-    ol = req.app['ol']
-    log = req.app['log_path']
+    nmt = req.app["nmt"]
+    pipeline = req.app["pipeline"]
+    pipeline_tgt = req.app["pipeline_tgt"]
+    lock = req.app["lock"]
+    ol = req.app["ol"]
+    log = req.app["log_path"]
 
     try:
         if not ol:
-            raise Exception('Online Learning is not active.')
-        req = await req.json() # TODO sensitive point
+            raise Exception("Online Learning is not active.")
+        req = await req.json()  # TODO sensitive point
+        num_steps = req.get("steps", 2)
         async with lock:
-            for tu in req['tus']:
-                if tu['tgt'] == '':
-                    raise Exception('Missing target segment for Online '\
-                        'Learning Training.')
-                src_preprocessed = pipeline.preprocess_str(tu['src'])
-                tgt_preprocessed = pipeline_tgt.preprocess_str(tu['tgt'])
-                named_tuple = time.localtime() # get struct_time
+            for tu in req.get("tus"):
+                if tu["tgt"] == "":
+                    raise Exception(
+                        "Missing target segment for Online " "Learning Training."
+                    )
+                src_preprocessed = pipeline.preprocess_str(tu["src"])
+                tgt_preprocessed = pipeline_tgt.preprocess_str(tu["tgt"])
+                named_tuple = time.localtime()  # get struct_time
                 time_string = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
-                with open(log, 'a+') as file:
-                    words = ['source:', 'source_prep:', 'target:',
-                        'target_prep:', 'score:']
-                    file.write(f'----Training----\n'\
-                        f'{time_string}\n'\
-                        f'{words[0]:>13} {tu["src"]}\n'\
-                        f'{words[1]:>13} {src_preprocessed}\n'\
-                        f'{words[2]:>13} {tu["tgt"]}\n'\
-                        f'{words[3]:>13} {tgt_preprocessed}\n\n')
-                nmt.train(src_preprocessed, tgt_preprocessed)
-        resp = {
-            'rc': 0
-        }
+                with open(log, "a+") as file:
+                    words = [
+                        "source:",
+                        "source_prep:",
+                        "target:",
+                        "target_prep:",
+                        "score:",
+                    ]
+                    file.write(
+                        f"----Training----\n"
+                        f"{time_string}\n"
+                        f'{words[0]:>13} {tu["src"]}\n'
+                        f"{words[1]:>13} {src_preprocessed}\n"
+                        f'{words[2]:>13} {tu["tgt"]}\n'
+                        f"{words[3]:>13} {tgt_preprocessed}\n\n"
+                    )
+                nmt.train(src_preprocessed, tgt_preprocessed, num_steps)
+        resp = {"rc": 0}
         return web.json_response(resp, status=200)
 
     except Exception as e:
-        response_obj = {'status': 'failed', 'reason': str(e)}
-        with open(log, 'a+') as file:
+        response_obj = {"status": "failed", "reason": str(e)}
+        with open(log, "a+") as file:
             named_tuple = time.localtime()
             time_string = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
-            file.write(f'--Failed Train--\n'\
-                f'{time_string}\n'\
-                f'reason: {str(e)}\n\n')
+            file.write(f"--Failed Train--\n" f"{time_string}\n" f"reason: {str(e)}\n\n")
         return web.json_response(response_obj, status=500)
