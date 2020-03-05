07f159e5351fbb191c98a2c4ce6394a0dce2bb6d
semantic-release <semantic-release>
2020-03-05 15:42:36 +0100

    3.45.0
    Automatically generated by python-semantic-release

--- pangeamt_toolkit/__init__.py
+++ pangeamt_toolkit/__init__.py
@@ -14,4 +14,4 @@ from pangeamt_toolkit.pangeanmt import Pangeanmt, PangeanmtServer
 import sys
 import onmt
 sys.modules['pangeamt_toolkit.onmt'] = onmt
-__version__ = '3.44.0'
+__version__ = '3.45.0'
